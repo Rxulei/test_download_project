e23d64468587a8958efa13e2295fc4e3d349a684
Rxulei <rxulei@qq.com>
2020-11-05 10:32:15 +0800

    添加了一个类：Append

---
+++ Append.java
@@ -0,0 +1,9 @@
+/**
+ * @author xulei
+ * @date 2020/11/5 - 10:29
+ */
+public class Append {
+    public static void main(String[] args) {
+        System.out.println("这是一个在本地新添加的类，待会先commit，再push，就能上传到GItHub了！");
+    }
+}
